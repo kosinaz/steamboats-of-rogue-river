c008190492de34ddd63aaec145e154d63413edcc
Kosina Zoltán <kosinaz@gmail.com>
2024-01-04 22:25:48 +0100

    add animated river

--- Steamboats of Rogue River/scenes/line.gd
+++ Steamboats of Rogue River/scenes/line.gd
@@ -1,4 +1,4 @@
-extends Path2D
+extends Line2D
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,9 @@ extends Path2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(curve)
+	$Path2D.curve.get_baked_nodes
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	$PathFollow2D.offset += 250 * delta
+#func _process(delta):
+#	pass
--- Steamboats of Rogue River/scenes/world.gd
+++ Steamboats of Rogue River/scenes/world.gd
@@ -1,4 +1,4 @@
-extends Path2D
+extends Node2D
 
 
 # Declare member variables here. Examples:
@@ -8,9 +8,9 @@ extends Path2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(curve)
+	$Line2D.points = $Path2D.curve.get_baked_points()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$PathFollow2D.offset += 250 * delta
+	$Path2D/PathFollow2D.offset += 250 * delta
